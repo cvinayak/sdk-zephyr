cvinayak/zephyr

Bluetooth: Controller: nRF54H20 compile error fixes
FICR compile error fixed
ECB compile error fixed
SWI compile error fixed
draft modification for radio
Update radio.c due to miss some device check

Signed-off-by: Jui-Chou Chung <jui-chou.chung@nordicsemi.no>
Signed-off-by: Vinayak Kariappa Chettimada <vich@nordicsemi.no>

subsys/bluetooth/controller/ll_sw/nordic/hal/nrf5/radio/radio_nrf5_dppi.h
@@ -7,6 +7,8 @@
 
 #if defined(CONFIG_SOC_COMPATIBLE_NRF54LX)
 #define NRF_DPPIC NRF_DPPIC10
+#elif defined(CONFIG_SOC_SERIES_NRF54HX)
+#define NRF_DPPIC NRF_DPPIC020
 #endif /* CONFIG_SOC_COMPATIBLE_NRF54LX */
 
 static inline void hal_radio_nrf_ppi_channels_enable(uint32_t mask)

subsys/bluetooth/controller/ll_sw/nordic/hal/nrf5/swi.h
@@ -41,7 +41,7 @@
 #error Unknown NRF5340 CPU.
 #endif /* !CONFIG_SOC_COMPATIBLE_NRF5340_CPUNET */
 
-/* nRF54 Series IRQ mapping */
+/* nRF54L Series IRQ mapping */
 #elif defined(CONFIG_SOC_COMPATIBLE_NRF54LX)
 
 #define HAL_SWI_RADIO_IRQ  SWI02_IRQn
@@ -61,6 +61,25 @@
 #define HAL_SWI_JOB_IRQ    SWI03_IRQn
 #endif
 
+/* nRF54H Series IRQ mapping */
+#elif defined(CONFIG_SOC_SERIES_NRF54HX)
+
+#define HAL_SWI_RADIO_IRQ  SWI2_IRQn
+
+#if defined(CONFIG_BT_CTLR_NRF_GRTC)
+#define HAL_SWI_WORKER_IRQ GRTC_1_IRQn
+#define HAL_RTC_IRQn       GRTC_1_IRQn
+#else /* !CONFIG_BT_CTLR_NRF_GRTC */
+#define HAL_SWI_WORKER_IRQ RTC_IRQn
+#define HAL_RTC_IRQn       RTC_IRQn
+#endif /* !CONFIG_BT_CTLR_NRF_GRTC */
+
+#if !defined(CONFIG_BT_CTLR_LOW_LAT) && \
+	(CONFIG_BT_CTLR_ULL_HIGH_PRIO == CONFIG_BT_CTLR_ULL_LOW_PRIO)
+#define HAL_SWI_JOB_IRQ    HAL_SWI_WORKER_IRQ
+#else
+#define HAL_SWI_JOB_IRQ    SWI3_IRQn
+#endif
 #endif
 
 static inline void hal_swi_init(void)

subsys/bluetooth/controller/ll_sw/nordic/hci/hci_vendor.c
@@ -10,6 +10,13 @@
 
 #include <soc.h>
 
+#if defined(CONFIG_SOC_SERIES_NRF54HX)
+#define DEVICEADDR BLE.ADDR
+#define DEVICEADDRTYPE BLE.ADDRTYPE
+#define IR BLE.IR
+#define ER BLE.ER
+#endif
+
 uint8_t hci_vendor_read_static_addr(struct bt_hci_vs_static_addr addrs[],
 				 uint8_t size)
 {

cvinayak/sdk-zephyr

subsys/bluetooth/controller/ll_sw/nordic/hal/nrf5/radio/radio_nrf5_dppi.h
@@ -7,6 +7,8 @@
 
 #if defined(CONFIG_SOC_COMPATIBLE_NRF54LX)
 #define NRF_DPPIC NRF_DPPIC10
+#elif defined(CONFIG_SOC_SERIES_NRF54H)
+#define NRF_DPPIC NRF_DPPIC020
 #endif /* CONFIG_SOC_COMPATIBLE_NRF54LX */
 
 static inline void hal_radio_nrf_ppi_channels_enable(uint32_t mask)

subsys/bluetooth/controller/ll_sw/nordic/hal/nrf5/swi.h
@@ -41,7 +41,7 @@
 #error Unknown NRF5340 CPU.
 #endif /* !CONFIG_SOC_COMPATIBLE_NRF5340_CPUNET */
 
-/* nRF54 Series IRQ mapping */
+/* nRF54L Series IRQ mapping */
 #elif defined(CONFIG_SOC_COMPATIBLE_NRF54LX)
 
 #define HAL_SWI_RADIO_IRQ  SWI02_IRQn
@@ -61,6 +61,25 @@
 #define HAL_SWI_JOB_IRQ    SWI03_IRQn
 #endif
 
+/* nRF54H Series IRQ mapping */
+#elif defined(CONFIG_SOC_SERIES_NRF54H)
+
+#define HAL_SWI_RADIO_IRQ  SWI2_IRQn
+
+#if defined(CONFIG_BT_CTLR_NRF_GRTC)
+#define HAL_SWI_WORKER_IRQ GRTC_1_IRQn
+#define HAL_RTC_IRQn       GRTC_1_IRQn
+#else /* !CONFIG_BT_CTLR_NRF_GRTC */
+#define HAL_SWI_WORKER_IRQ RTC_IRQn
+#define HAL_RTC_IRQn       RTC_IRQn
+#endif /* !CONFIG_BT_CTLR_NRF_GRTC */
+
+#if !defined(CONFIG_BT_CTLR_LOW_LAT) && \
+	(CONFIG_BT_CTLR_ULL_HIGH_PRIO == CONFIG_BT_CTLR_ULL_LOW_PRIO)
+#define HAL_SWI_JOB_IRQ    HAL_SWI_WORKER_IRQ
+#else
+#define HAL_SWI_JOB_IRQ    SWI3_IRQn
+#endif
 #endif
 
 static inline void hal_swi_init(void)

subsys/bluetooth/controller/ll_sw/nordic/hci/hci_vendor.c
@@ -10,6 +10,13 @@
 
 #include <soc.h>
 
+#if defined(CONFIG_SOC_SERIES_NRF54H)
+#define DEVICEADDR BLE.ADDR
+#define DEVICEADDRTYPE BLE.ADDRTYPE
+#define IR BLE.IR
+#define ER BLE.ER
+#endif
+
 uint8_t hci_vendor_read_static_addr(struct bt_hci_vs_static_addr addrs[],
 				 uint8_t size)
 {