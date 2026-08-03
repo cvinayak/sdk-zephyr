zephyrproject-rtos/zephyr

samples: Bluetooth: Observer plus Broadcaster Multiple
Add a sample combining Observer, multiple Advertising sets
Broadcaster and included connectable advertising to support
simultaneous Peripheral role.

Signed-off-by: Vinayak Kariappa Chettimada <vich@nordicsemi.no>

samples/bluetooth/observer/src/observer.c
@@ -12,11 +12,13 @@
 static void device_found(const bt_addr_le_t *addr, int8_t rssi, uint8_t type,
 			 struct net_buf_simple *ad)
 {
+#if !defined(CONFIG_BT_EXT_ADV)
 	char addr_str[BT_ADDR_LE_STR_LEN];
 
 	bt_addr_le_to_str(addr, addr_str, sizeof(addr_str));
 	printk("Device found: %s (RSSI %d), type %u, AD data len %u\n",
 	       addr_str, rssi, type, ad->len);
+#endif /* !CONFIG_BT_EXT_ADV */
 }
 
 #if defined(CONFIG_BT_EXT_ADV)
@@ -53,6 +55,7 @@ static const char *phy2str(uint8_t phy)
 static void scan_recv(const struct bt_le_scan_recv_info *info,
 		      struct net_buf_simple *buf)
 {
+	static uint32_t rx_cnt;
 	char le_addr[BT_ADDR_LE_STR_LEN];
 	char name[NAME_LEN];
 	uint8_t data_status;
@@ -66,10 +69,13 @@ static void scan_recv(const struct bt_le_scan_recv_info *info,
 	data_status = BT_HCI_LE_ADV_EVT_TYPE_DATA_STATUS(info->adv_props);
 
 	bt_addr_le_to_str(info->addr, le_addr, sizeof(le_addr));
-	printk("[DEVICE]: %s, AD evt type %u, Tx Pwr: %i, RSSI %i "
+
+	rx_cnt++;
+
+	printk("%u. [DEVICE]: %s, AD evt type %u, Tx Pwr: %i, RSSI %i "
 	       "Data status: %u, AD data len: %u Name: %s "
 	       "C:%u S:%u D:%u SR:%u E:%u Pri PHY: %s, Sec PHY: %s, "
-	       "Interval: 0x%04x (%u ms), SID: %u\n",
+	       "Interval: 0x%04x (%u ms), SID: %u\n", rx_cnt,
 	       le_addr, info->adv_type, info->tx_power, info->rssi,
 	       data_status, data_len, name,
 	       (info->adv_props & BT_GAP_ADV_PROP_CONNECTABLE) != 0,
@@ -91,9 +97,9 @@ static int scan_start(void)
 	/* 30 ms continuous active scanning with duplicate filtering. */
 	struct bt_le_scan_param scan_param = {
 		.type       = BT_LE_SCAN_TYPE_ACTIVE,
-		.options    = BT_LE_SCAN_OPT_FILTER_DUPLICATE,
-		.interval   = BT_GAP_SCAN_FAST_INTERVAL_MIN,
-		.window     = BT_GAP_SCAN_FAST_WINDOW,
+		.options    = BT_LE_SCAN_OPT_CODED,
+		.interval   = 0x0010,
+		.window     = 0x0010,
 	};
 	int err;
 
@@ -124,6 +130,25 @@ scan_start_retry:
 	return 0;
 }
 
+static struct k_work_delayable scan_stop_work;
+
+static void scan_stop_timeout(struct k_work *work)
+{
+	int err;
+
+	printk("Stopping scanning...\n");
+	err = bt_le_scan_stop();
+	if (err) {
+		printk("Stop scanning failed (err %d)\n", err);
+		return;
+	}
+	printk("Stopped scanning...\n");
+
+	(void)scan_start();
+
+	k_work_schedule(&scan_stop_work, K_SECONDS(5));
+}
+
 int observer_start(void)
 {
 	int err;
@@ -137,6 +162,9 @@ int observer_start(void)
 	if (err != 0) {
 		return err;
 	}
+
+	k_work_init_delayable(&scan_stop_work, scan_stop_timeout);
+	k_work_schedule(&scan_stop_work, K_MSEC(0));
 
 	return 0;
 }

cvinayak/sdk-zephyr

samples/bluetooth/observer/src/observer.c
@@ -12,8 +12,10 @@
 static void device_found(const bt_addr_le_t *addr, int8_t rssi, uint8_t type,
 			 struct net_buf_simple *ad)
 {
+#if !defined(CONFIG_BT_EXT_ADV)
 	printk("Device found: %s (RSSI %d), type %u, AD data len %u\n",
 	       bt_addr_le_str(addr), rssi, type, ad->len);
+#endif /* !CONFIG_BT_EXT_ADV */
 }
 
 #if defined(CONFIG_BT_EXT_ADV)
@@ -50,6 +52,7 @@ static const char *phy2str(uint8_t phy)
 static void scan_recv(const struct bt_le_scan_recv_info *info,
 		      struct net_buf_simple *buf)
 {
+	static uint32_t rx_cnt;
 	char name[NAME_LEN];
 	uint8_t data_status;
 	uint16_t data_len;
@@ -61,10 +64,12 @@ static void scan_recv(const struct bt_le_scan_recv_info *info,
 
 	data_status = BT_HCI_LE_ADV_EVT_TYPE_DATA_STATUS(info->adv_props);
 
-	printk("[DEVICE]: %s, AD evt type %u, Tx Pwr: %i, RSSI %i "
+	rx_cnt++;
+
+	printk("%u. [DEVICE]: %s, AD evt type %u, Tx Pwr: %i, RSSI %i "
 	       "Data status: %u, AD data len: %u Name: %s "
 	       "C:%u S:%u D:%u SR:%u E:%u Pri PHY: %s, Sec PHY: %s, "
-	       "Interval: 0x%04x (%u ms), SID: %u\n",
+	       "Interval: 0x%04x (%u ms), SID: %u\n", rx_cnt,
 	       bt_addr_le_str(info->addr), info->adv_type, info->tx_power, info->rssi,
 	       data_status, data_len, name,
 	       (info->adv_props & BT_GAP_ADV_PROP_CONNECTABLE) != 0,
@@ -86,9 +91,9 @@ static int scan_start(void)
 	/* 30 ms continuous active scanning with duplicate filtering. */
 	struct bt_le_scan_param scan_param = {
 		.type       = BT_LE_SCAN_TYPE_ACTIVE,
-		.options    = BT_LE_SCAN_OPT_FILTER_DUPLICATE,
-		.interval   = BT_GAP_SCAN_FAST_INTERVAL_MIN,
-		.window     = BT_GAP_SCAN_FAST_WINDOW,
+		.options    = BT_LE_SCAN_OPT_NONE,
+		.interval   = 0x0010,
+		.window     = 0x0010,
 	};
 	int err;
 
@@ -119,6 +124,25 @@ scan_start_retry:
 	return 0;
 }
 
+static struct k_work_delayable scan_stop_work;
+
+static void scan_stop_timeout(struct k_work *work)
+{
+	int err;
+
+	printk("Stopping scanning...\n");
+	err = bt_le_scan_stop();
+	if (err) {
+		printk("Stop scanning failed (err %d)\n", err);
+		return;
+	}
+	printk("Stopped scanning...\n");
+
+	(void)scan_start();
+
+	k_work_schedule(&scan_stop_work, K_SECONDS(5));
+}
+
 int observer_start(void)
 {
 	int err;
@@ -132,6 +156,9 @@ int observer_start(void)
 	if (err != 0) {
 		return err;
 	}
+
+	k_work_init_delayable(&scan_stop_work, scan_stop_timeout);
+	k_work_schedule(&scan_stop_work, K_MSEC(0));
 
 	return 0;
 }