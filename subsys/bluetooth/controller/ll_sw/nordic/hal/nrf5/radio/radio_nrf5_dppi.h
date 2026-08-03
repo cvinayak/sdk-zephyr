zephyrproject-rtos/zephyr

Bluetooth: Controller: nrf54hx: Initial GRTC support
Added initial implementation for nrf54hx GRTC use for Radio.

Signed-off-by: Vinayak Kariappa Chettimada <vich@nordicsemi.no>

subsys/bluetooth/controller/ll_sw/nordic/hal/nrf5/radio/radio_nrf5_dppi.h
@@ -108,6 +108,7 @@ static inline void hal_event_timer_start_ppi_config(void)
 	nrf_grtc_publish_set(NRF_GRTC, HAL_CNTR_GRTC_EVENT_COMPARE_RADIO,
 			     HAL_EVENT_TIMER_START_PPI);
 
+#if defined(CONFIG_SOC_COMPATIBLE_NRF54LX)
 	/* Setup PPIB receive publish */
 	nrf_ppib_publish_set(NRF_PPIB11, HAL_PPIB_RECEIVE_EVENT_TIMER_START_PPI,
 			     HAL_EVENT_TIMER_START_PPI);
@@ -117,8 +118,26 @@ static inline void hal_event_timer_start_ppi_config(void)
 			       HAL_EVENT_TIMER_START_PPI);
 
 	/* Enable same DPPI in Peripheral domain */
-	nrf_dppi_channels_enable(NRF_DPPIC20,
-				 BIT(HAL_EVENT_TIMER_START_PPI));
+	nrf_dppi_channels_enable(NRF_DPPIC20, BIT(HAL_EVENT_TIMER_START_PPI));
+
+#elif defined(CONFIG_SOC_SERIES_NRF54HX)
+	/* Setup IPCT receive publish in radio domain */
+	nrf_ipct_publish_set(NRF_IPCT, HAL_IPCT_RECEIVE_EVENT_TIMER_START_PPI,
+			       HAL_EVENT_TIMER_START_PPI);
+
+	/* Setup IPCT send subscribe in main domain */
+	nrf_ipct_subscribe_set(NRF_IPCT130, HAL_IPCT_SEND_EVENT_TIMER_START_PPI,
+			       HAL_EVENT_TIMER_START_PPI);
+
+	nrf_ipct_shorts_enable(NRF_IPCT, IPCT_SHORTS_RECEIVE0_FLUSH0_Msk);
+
+	/* Enable same DPPI in main domain */
+	nrf_dppi_channels_enable(NRF_DPPIC130, BIT(HAL_EVENT_TIMER_START_PPI));
+
+	/* Enable same DPPI in global domain */
+	nrf_dppi_channels_enable(NRF_DPPIC132, BIT(HAL_EVENT_TIMER_START_PPI));
+#else
+#endif
 
 #else /* !CONFIG_BT_CTLR_NRF_GRTC */
 	nrf_rtc_publish_set(NRF_RTC, NRF_RTC_EVENT_COMPARE_2, HAL_EVENT_TIMER_START_PPI);

cvinayak/sdk-zephyr

subsys/bluetooth/controller/ll_sw/nordic/hal/nrf5/radio/radio_nrf5_dppi.h
@@ -199,6 +199,7 @@ static inline void hal_event_timer_start_ppi_config(void)
 	nrf_grtc_publish_set(NRF_GRTC, HAL_CNTR_GRTC_EVENT_COMPARE_RADIO,
 			     HAL_EVENT_TIMER_START_PPI);
 
+#if defined(CONFIG_SOC_COMPATIBLE_NRF54LX)
 	/* Setup PPIB receive publish for Radio domain (PPIB11/PPIB21):
 	 * GRTC (DPPIC20) -> NRF_TIMER10 (DPPIC10).
 	 * In single-timer mode NRF_TIMER10 is the EVENT_TIMER; in dual-timer mode
@@ -236,6 +237,25 @@ static inline void hal_event_timer_start_ppi_config(void)
 	/* Setup TIMER start subscribe in radio domain */
 	nrf_timer_subscribe_set(EVENT_TIMER, NRF_TIMER_TASK_START, HAL_EVENT_TIMER_START_PPI);
 #endif /* CONFIG_BT_CTLR_SW_SWITCH_SINGLE_TIMER */
+
+#elif defined(CONFIG_SOC_SERIES_NRF54H)
+	/* Setup IPCT receive publish in radio domain */
+	nrf_ipct_publish_set(NRF_IPCT, HAL_IPCT_RECEIVE_EVENT_TIMER_START_PPI,
+			     HAL_EVENT_TIMER_START_PPI);
+
+	/* Setup IPCT send subscribe in main domain */
+	nrf_ipct_subscribe_set(NRF_IPCT130, HAL_IPCT_SEND_EVENT_TIMER_START_PPI,
+			       HAL_EVENT_TIMER_START_PPI);
+
+	nrf_ipct_shorts_enable(NRF_IPCT, IPCT_SHORTS_RECEIVE0_FLUSH0_Msk);
+
+	/* Enable same DPPI in main domain */
+	nrf_dppi_channels_enable(NRF_DPPIC130, BIT(HAL_EVENT_TIMER_START_PPI));
+
+	/* Enable same DPPI in global domain */
+	nrf_dppi_channels_enable(NRF_DPPIC132, BIT(HAL_EVENT_TIMER_START_PPI));
+#else
+#endif
 
 #else /* !CONFIG_BT_CTLR_NRF_GRTC */
 	nrf_rtc_publish_set(NRF_RTC, NRF_RTC_EVENT_COMPARE_2, HAL_EVENT_TIMER_START_PPI);